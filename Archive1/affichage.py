088cc420fd61da8b02e647dfda93e4e1db7775f4
kevinushaka <keushakaku@outlook.com>
2020-10-27 10:28:52 +0100

    Archive rendu 3

--- Archive1/affichage.py
+++ Archive1/affichage.py
@@ -1,6 +1,6 @@
 'Aucun = 0' 
 'tkinter = 1'  # Not implemented
-OpenCV = 2  # Not completly implemented
+'OpenCV = 2'  # Not completly implemented
 'PERSONNE = (0.0, 0.0, 1.0)' 
 'IMMOBILE = (1.0, 0.0, 0.0)' 
 'OBSTACLE = (0.0, 1.0, 0.0)' 
